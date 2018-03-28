96cdda2a3e05d72b0e0aa862d394d76d0068f65c
Sergio Xalambrí <sergiodxa@gmail.com>
2018-03-28 17:37:32 -0500

    Add createdAt date

--- index.js
+++ index.js
@@ -35,10 +35,13 @@ function saveOnDB(db, { type, ...document }) {
   const collection = db.collection(type);
 
   return new Promise((resolve, reject) => {
-    collection.insert([document], (error, respoonse) => {
-      if (error) return reject(error);
-      resolve(respoonse);
-    });
+    collection.insert(
+      [Object.assign(document, { createdAt: Date.now() })],
+      (error, respoonse) => {
+        if (error) return reject(error);
+        resolve(respoonse);
+      }
+    );
   });
 }
 
